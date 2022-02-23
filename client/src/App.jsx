0b09b9761c84205fc2b88905fffae9dae6cdb0ae
Miro Manestar <miroimanestar@gmail.com>
2022-02-23 00:35:25 -0500

    Continued work on face api integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react'
+import Webcam from 'react-webcam'
 import './App.css'
 
-function App() {
-    const [count, setCount] = useState(0)
+import '@tensorflow/tfjs'
+import * as canvas from 'canvas'
+import * as faceapi from 'face-api.js'
 
+const mtcnnForwardParams = {
+    minFaceSize: 200,
+}
+
+function App() {
+    const [stream, setStream] = useState(null)
+    const webcamRef = useRef(null)
+
+
+    const options = new faceapi.MtcnnOptions()
+    const faceDetection = async () => {
+        console.log('Detections')
+        await faceapi.loadMtcnnModel('/models')
+        await faceapi.loadFaceDetectionModel('/models')
+    }
+
+    useEffect(() => {
+        setInterval(async () => {
+            const detections = await faceapi.detectAllFaces(webcamRef.current.video, options)
+        }, 20)
+    }, [])
     return (
         <div className="App">
-            NOTHING HERE
+            <Webcam
+                audio={false}
+                ref={webcamRef}
+                onUserMedia={() => faceDetection()}
+            />
         </div>
     )
 }
