6b6a5dd60981d3091849a46b5dd5a286d9ce2879
Miro Manestar <miroimanestar@gmail.com>
2022-03-16 18:24:52 -0400

    Worked on facial recognition

--- electron/packages/renderer/src/views/Camera.tsx
+++ electron/packages/renderer/src/views/Camera.tsx
@@ -19,40 +19,82 @@ const useStyles = createUseStyles({
     }
 })
 
+const MAX_FRAMERATE = 60
+
 const Camera = () => {
     const classes = useStyles()
 
     const webcamRef: any = useRef(null)
     const canvasRef: any = useRef(null)
+    const framerate: any = useRef({ fr: 0, count: 0})
 
-    
-    const beginDetection = async (stream: MediaStream) => {
-        // console.log(canvas.width, canvas.height)
-        // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        // const video = webcamRef.current.getScreenshot()
-        // const canvas: any = faceapi.createCanvasFromMedia(video)
-        // console.log(canvas)
+    let temp: any = null
 
-        await faceapi.loadTinyFaceDetectorModel('/models')
-        await faceapi.loadFaceLandmarkModel('/models')
-        await faceapi.loadFaceExpressionModel('/models')
-        await faceapi.loadFaceDetectionModel('./models')
+    const doDetection = async () => {
+        if (framerate.current.count >= MAX_FRAMERATE)
+            return
+
+        const detections = await faceapi.detectAllFaces(webcamRef.current.video, new faceapi.SsdMobilenetv1Options()).withFaceLandmarks().withFaceDescriptors()
+            
+        const videoEl = webcamRef.current.video
+        const displaySize = { width: videoEl?.clientWidth || 0, height: videoEl?.clientHeight || 0 }
+        faceapi.matchDimensions(canvasRef.current, displaySize)
+        const resizedDetections: any = faceapi.resizeResults(detections, displaySize)
+
+        faceapi.draw.drawDetections(canvasRef.current, resizedDetections)
+        //faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections)
+
+        const matcher = new faceapi.FaceMatcher(temp, 0.6)
+        const results = resizedDetections.map((d: any) => matcher.findBestMatch(d.descriptor))
+        
+        results.forEach((bestMatch: any, i: any) => {
+            const box = resizedDetections[i].detection.box
+            const text = bestMatch.toString()
+            const drawBox = new faceapi.draw.DrawBox(box, { label: text })
+            drawBox.draw(canvasRef.current)
+        })
+
+        framerate.current.count++
+        let ctx = canvasRef.current.getContext('2d')
+        ctx.font = '30px Arial'
+        ctx.fillStyle = '#000'
+        ctx.fillText(`${framerate.current.fr} FPS`, 10, 30)
+
     }
 
     useEffect(() => {
-        setInterval(async () => {
-            const detections = await faceapi.detectAllFaces(webcamRef.current.video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-            const videoEl = document.querySelector(`.${classes.root} video`)
+        
+        (async () => { 
+            await faceapi.loadTinyFaceDetectorModel('/models')
+            await faceapi.loadSsdMobilenetv1Model('/models')
+            await faceapi.loadFaceLandmarkModel('/models')
+            await faceapi.loadFaceDetectionModel('./models')
+            await faceapi.loadFaceRecognitionModel('./models')
 
-            const displaySize = { width: videoEl?.clientWidth || 0, height: videoEl?.clientHeight || 0 }
-            faceapi.matchDimensions(canvasRef.current, displaySize)
-            const resizedDetections: any = faceapi.resizeResults(detections, displaySize)
+            const img = await faceapi.fetchImage('https://media-exp1.licdn.com/dms/image/C4D03AQGg2lhu0LMXrA/profile-displayphoto-shrink_800_800/0/1589210134416?e=1652918400&v=beta&t=pXyID_PClzCQQavvyyzaL0Bn3ubd4cPKojACc9Rm40Q')
+            const desc = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
 
-            faceapi.draw.drawDetections(canvasRef.current, resizedDetections)
-            faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections)
-            faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections)
-        }, 20)
-    })
+            if (!desc)
+                throw new Error('No face detected')
+
+            temp = new faceapi.LabeledFaceDescriptors('Miro', [desc.descriptor])
+
+            console.log('Models loaded')
+            setTimeout(async () => { 
+                console.log('Beginning detection')
+                
+                while (true)
+                    await doDetection()
+            }, 300)
+        })()
+
+        console.log('Loading models...')
+
+        setInterval(() => {
+            framerate.current.fr = framerate.current.count
+            framerate.current.count = 0
+        }, 1000)
+    }, [])
 
     return (
         <div className={classes.root}>
@@ -60,7 +102,10 @@ const Camera = () => {
                 className={classes.webcam}
                 audio={false}
                 ref={webcamRef}
-                onUserMedia={(stream) => beginDetection(stream)}
+                onUserMedia={(stream) => console.log('Camera started')}
+                // videoConstraints={{
+                //     frameRate: { max: 20 },
+                // }}
             />
             <canvas className={classes.canvas} ref={canvasRef} />
         </div >
